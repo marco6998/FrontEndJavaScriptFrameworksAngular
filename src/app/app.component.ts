5e0b10d680e4755859ba042f17ccc7d5faaa1e95
Marco P <mjpf99@hotmail.com>
2021-04-24 22:10:34 -0500

    Configuring Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html', /*Apunta a un html que contiene la plantilla correspondiente, */
+  styleUrls: ['./app.component.scss'] 
 })
 export class AppComponent {
   title = 'conFusion';
